20dea00c3e1d953f224852c9caf35baaa595837d
sugimo <sugimo.324@gmail.com>
2023-02-19 05:04:26 +0900

    refactor server run

--- main.go
+++ main.go
@@ -5,19 +5,12 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"net/http"
 	"os"
 
 	"github.com/sugimoto-ne/go_sample_app.git/config"
-	"golang.org/x/sync/errgroup"
 )
 
 func main() {
-	if len(os.Args) != 2 {
-		log.Printf("need port number\n")
-		os.Exit(1)
-	}
-
 	if err := run(context.Background()); err != nil {
 		log.Printf("failed to terminate server: %v", err)
 		os.Exit(1)
@@ -25,12 +18,10 @@ func main() {
 }
 
 func run(ctx context.Context) error {
-
 	cfg, err := config.New()
 	if err != nil {
 		return err
 	}
-
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
 	if err != nil {
 		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
@@ -38,39 +29,8 @@ func run(ctx context.Context) error {
 
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
+	mux := NewMux()
+	s := NewServer(l, mux)
 
-	s := &http.Server{
-		//Addrを指定せず動的にポートを割り当てる
-		// Addr: ":18080",
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Fprintf(w, "Hello, %s", r.URL.Path[1:])
-		}),
-	}
-
-	eg, ctx := errgroup.WithContext(ctx)
-
-	eg.Go(func() error {
-		fmt.Println("############################")
-		fmt.Println("another goroutine")
-		fmt.Println("############################")
-		//ListenAndServeではなくServeに変更
-		// if err := s.ListenAndServe(); err != nil &&
-		if err := s.Serve(l); err != nil &&
-			err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-
-			return err
-		}
-
-		return nil
-	})
-
-	fmt.Println("waiting done")
-	<-ctx.Done()
-	fmt.Println("Done !!")
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-
-	return eg.Wait()
+	return s.Run(ctx)
 }
