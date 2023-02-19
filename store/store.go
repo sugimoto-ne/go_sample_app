e87ee33c48b79495179e9ff26a0e7179f4b64354
sugimo <sugimo.324@gmail.com>
2023-02-20 00:22:04 +0900

    add tasks endpoint

---
+++ store/store.go
@@ -0,0 +1,34 @@
+package store
+
+import (
+	"errors"
+
+	"github.com/sugimoto-ne/go_sample_app.git/entity"
+)
+
+var (
+	Tasks = &TaskStore{Tasks: map[entity.TaskID]*entity.Task{}}
+
+	ErrNotFound = errors.New("not found")
+)
+
+type TaskStore struct {
+	LastID entity.TaskID
+	Tasks  map[entity.TaskID]*entity.Task
+}
+
+func (ts *TaskStore) Add(t *entity.Task) (entity.TaskID, error) {
+	ts.LastID++
+	ts.Tasks[t.ID] = t
+
+	return t.ID, nil
+}
+
+func (ts *TaskStore) All() entity.Tasks {
+	tasks := make([]*entity.Task, len(ts.Tasks))
+	for i, t := range ts.Tasks {
+		tasks[i-1] = t
+	}
+
+	return tasks
+}
